a4269a65a882ac0912a6f4495bdd41dfab47f130
rasvn <101136699+rasvn@users.noreply.github.com>
2022-03-20 18:49:07 +0200

    Delete Tema 2 directory

--- Tema 2/Tema 2/user_interface.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/*
-* Se porneste programul cu interfata de tip consola
-* return - un meniu prin care utilizatorul poate sa aleaga
-*		ce doreste sa execute programul
-*/
-void run();
